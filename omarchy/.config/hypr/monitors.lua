7ef0d534ff09d68cc94ac8bb067eb81f9ec88d81
Luis David Garcia Lunar <ldgl1215@gmail.com>
2026-08-19 17:42:23 -0300

    set monitors

--- omarchy/.config/hypr/monitors.lua
+++ omarchy/.config/hypr/monitors.lua
@@ -1,15 +1,15 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 -- List current monitors and supported resolutions with: hyprctl monitors all
 
-local omarchy_gdk_scale = 2
-local omarchy_monitor_scale = "auto"
+local omarchy_gdk_scale = 1
+local omarchy_monitor_scale = 1.25
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
+--hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
 
 -- Configure a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
-hl.monitor({ output = "DP-1", mode = "2560x1440@180Hz", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-1", mode = "2560x1440@180Hz", position = "0x0", scale = omarchy_monitor_scale })
 hl.monitor({ output = "DP-2", disabled = true })
 
 -- Portrait/rotated secondary monitor (transform: 1 = 90°, 3 = 270°).
